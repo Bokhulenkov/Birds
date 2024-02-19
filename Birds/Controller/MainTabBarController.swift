75811c1f56e811acc2825499de3bcbf29d28fded
Bokhulenkov <bokhulenkov@gmail.com>
2024-02-19 08:33:06 +0300

    create: TabBarController

---
+++ Birds/Controller/MainTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabBarController.swift
+//  Birds
+//
+//  Created by Alexander Bokhulenkov on 19.02.2024.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        view.backgroundColor = .systemPink
+    }
+    
+
+
+}
