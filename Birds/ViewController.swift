41f19e81cab5d03dd577c3a89a2f97e270de8b24
Bokhulenkov <bokhulenkov@gmail.com>
2024-02-19 08:19:10 +0300

    Initial Commit

---
+++ Birds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Birds
+//
+//  Created by Alexander Bokhulenkov on 19.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
