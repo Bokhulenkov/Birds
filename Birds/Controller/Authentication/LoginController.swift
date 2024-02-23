1854d37e9aa6c0e4af6706b4808e497d0e08f3c4
Bokhulenkov <bokhulenkov@gmail.com>
2024-02-23 11:32:57 +0300

    add image authorisation to LoginController

--- Birds/Controller/Authentication/LoginController.swift
+++ Birds/Controller/Authentication/LoginController.swift
@@ -18,6 +18,34 @@ final class LoginController: UIViewController {
         return iv
     }()
     
+    private lazy var emailContainerView: UIView = {
+       let view = UIView()
+        view.backgroundColor = .red
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        let iv = UIImageView()
+        iv.image = UIImage(named: "mail")
+        view.addSubview(iv)
+        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 8)
+        iv.setDimensions(width: 24, height: 24)
+        
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemPink
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        let iv = UIImageView()
+        iv.image = UIImage(named: "ic_lock_outline_white_2x")
+        view.addSubview(iv)
+        iv.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, paddingLeft: 8, paddingBottom: 10)
+        iv.setDimensions(width: 24, height: 24)
+        
+        return view
+    }()
+    
 //    MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,6 +65,13 @@ final class LoginController: UIViewController {
         view.addSubview(logoImageView)
         logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor, paddingTop: 0)
         logoImageView.setDimensions(width: 150.0, height: 150.0)
+        
+//        create stack view
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+        stack.axis = .vertical
+        stack.spacing = 8
+        view.addSubview(stack)
+        stack.anchor(top: logoImageView.bottomAnchor, left: view.safeAreaLayoutGuide.leftAnchor, right: view.safeAreaLayoutGuide.rightAnchor)
     }
     
 }
