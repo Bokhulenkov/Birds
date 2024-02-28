d230598ca09040c85f50e9192eae98bd05376da2
Bokhulenkov <bokhulenkov@gmail.com>
2024-02-28 15:10:54 +0300

    load images to FireBase

---
+++ Birds/Utillits/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Birds
+//
+//  Created by Alexander Bokhulenkov on 28.02.2024.
+//
+
+import Firebase
+import FirebaseStorage
+
+let STORAGE_REF = Storage.storage().reference()
+let STORAGE_PROFILE_IMAGES = STORAGE_REF.child("profile_images")
+
+let DB_REF = Database.database().reference()
+let REF_USERS = DB_REF.child("users")
